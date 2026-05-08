711e0888dbd20d778037fac6c1e6c4e8577e8546
karthikraja <karthikrajasenthivel@gmail.com>
2026-05-08 22:52:07 +0530

    Time: 863 ms (13.35%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,8 +1,3 @@
-# Write your MySQL query statement below
-
-# product id, year, quantity, and price 
-# first year of every product sold
-
 
 SELECT product_id, year AS first_year, quantity, price
 FROM Sales
