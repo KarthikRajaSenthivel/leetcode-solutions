7adec606635697c1023c01793d6642f2257e4f9d
karthikraja <karthikrajasenthivel@gmail.com>
2026-05-10 10:02:52 +0530

    Time: 1439 ms (7.32%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,1 @@
+select distinct p.product_id,coalesce((select p2.new_price from Products p2 where p2.product_id=p.product_id and p2.change_date <='2019-08-16' order by change_date desc limit 1),10) as price from products p
