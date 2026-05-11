d700975bf7d62d99ffdadb08e0af9c42d2e70b85
karthikraja <karthikrajasenthivel@gmail.com>
2026-05-11 09:52:11 +0530

    Time: 618 ms (83.78%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,6 +1,6 @@
 select Department, Employee, Salary from
 (
-    select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() over (partition by d.id order by e.salary desc) as r
+    select d.name as Department, e.name as Employee, e.salary as Salary, rank() over (partition by d.id order by e.salary desc) as r
     from Employee e
  join
     Department d 
